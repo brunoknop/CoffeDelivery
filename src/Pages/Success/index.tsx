4805d0579b892468c00723f3dbdc437393643e0f
brunoknop <brunog.knop@gmail.com>
2023-08-26 03:54:07 -0300

    finalizada o visual estático da página de sucesso

--- src/Pages/Success/index.tsx
+++ src/Pages/Success/index.tsx
@@ -1,7 +1,61 @@
+import {
+  AddressContainer,
+  DeliveryData,
+  DeliveryTime,
+  GradientBorderContainer,
+  InfoBox,
+  PaymentMethod,
+  SuccessContainer,
+  SuccessContentContainer,
+  SuccessTitleContainer,
+  TextContainer,
+} from './style'
+import SuccessIlustration from '../../assets/success-illustration.svg'
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
+
 export function Success() {
   return (
-    <div>
-      <h1>Success</h1>
-    </div>
+    <SuccessContainer>
+      <SuccessTitleContainer>
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+      </SuccessTitleContainer>
+      <SuccessContentContainer>
+        <GradientBorderContainer>
+          <DeliveryData>
+            <InfoBox>
+              <AddressContainer>
+                <MapPin size={16} weight="fill" />
+              </AddressContainer>
+              <TextContainer>
+                <p>
+                  Entrega em <span>Rua João Daniel Martinelli, 102</span>
+                </p>
+                <p>Farrapos - Porto Alegre, RS</p>
+              </TextContainer>
+            </InfoBox>
+            <InfoBox>
+              <DeliveryTime>
+                <Timer size={16} weight="fill" />
+              </DeliveryTime>
+              <TextContainer>
+                <p>Previsão de entrega</p>
+                <span>20 min - 30 min</span>
+              </TextContainer>
+            </InfoBox>
+            <InfoBox>
+              <PaymentMethod>
+                <CurrencyDollar size={16} />
+              </PaymentMethod>
+              <TextContainer>
+                <p>Pagamento na entrega</p>
+                <span>Cartão de Crédito</span>
+              </TextContainer>
+            </InfoBox>
+          </DeliveryData>
+        </GradientBorderContainer>
+        <img src={SuccessIlustration} alt="" />
+      </SuccessContentContainer>
+    </SuccessContainer>
   )
 }
