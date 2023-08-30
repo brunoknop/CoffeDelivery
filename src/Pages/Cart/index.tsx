fd5f080fc5918d73af96fb54efa9bd7d58d5c453
brunoknop <brunog.knop@gmail.com>
2023-08-29 22:19:15 -0300

    Estilização dos itens da lista

--- src/Pages/Cart/index.tsx
+++ src/Pages/Cart/index.tsx
@@ -1,17 +1,65 @@
-import { AddressAndPayment } from "./components/AddressAndPayment";
-import { CartItems } from "./components/CartItems";
+import { Trash } from '@phosphor-icons/react'
+import { Coffees } from '../../Coffee'
+import {
+  AddressAndPaymentContainer,
+  AddressContainer,
+  Description,
+  Handlers,
+  ItemContent,
+  MainContainer,
+  NameAndPrice,
+  OrderDetails,
+  OrderDetailsContainer,
+  OrderItem,
+  OrderValues,
+  PaymentContainer,
+} from './style'
 
 export function Cart() {
   return (
-    <div>
-      <div>
+    <MainContainer>
+      <AddressAndPaymentContainer>
         <h2>Complete seu pedido</h2>
-        <AddressAndPayment />
-      </div>
-      <div>
+        <AddressContainer>
+          <Description></Description>
+        </AddressContainer>
+        <PaymentContainer>
+          <Description></Description>
+        </PaymentContainer>
+      </AddressAndPaymentContainer>
+      <OrderDetailsContainer>
         <h2>Cafés Selecionados</h2>
-        <CartItems />
-      </div>
-    </div>
+        <OrderDetails>
+          {Coffees.map((coffee) => {
+            if (
+              coffee.id === '34db4331-eb67-4c94-8e62-706f22b75a1a' ||
+              coffee.id === 'eb2d3e51-1ac0-4c1d-8fef-a2578f782cd7'
+            )
+              return (
+                <OrderItem>
+                  <img src={coffee.img} alt="" />
+                  <ItemContent>
+                    <NameAndPrice>
+                      <p>{coffee.type}</p>
+                      <span>R$ {coffee.price}0</span>
+                    </NameAndPrice>
+                    <Handlers>
+                      <input type="number" />
+                      <button>
+                        <span>
+                          <Trash size={16} />
+                        </span>
+                        REMOVER
+                      </button>
+                    </Handlers>
+                  </ItemContent>
+                </OrderItem>
+              )
+            else return null
+          })}
+          <OrderValues></OrderValues>
+        </OrderDetails>
+      </OrderDetailsContainer>
+    </MainContainer>
   )
 }
--- src/Pages/Cart/style.ts
+++ src/Pages/Cart/style.ts
@@ -1,3 +1,116 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components'
 
-  export const AddressContainer = styled.div
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  h2 {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-family: 'Baloo 2';
+    font-size: 1.125rem;
+    font-weight: 700;
+    line-height: 130%;
+    margin-bottom: 0.9375rem;
+  }
+`
+
+export const AddressAndPaymentContainer = styled.div``
+export const AddressContainer = styled.form``
+export const PaymentContainer = styled.div``
+export const Description = styled.form``
+
+export const OrderDetailsContainer = styled.div``
+
+export const OrderDetails = styled.div`
+  display: flex;
+  width: 448px;
+  padding: 40px;
+  flex-direction: column;
+  border-radius: 6px 44px;
+  background: ${(props) => props.theme['base-card']};
+`
+
+export const OrderItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.25rem;
+
+  padding-bottom: 1.5rem;
+  border-bottom: 1px solid ${(props) => props.theme['base-button']};
+
+  + div {
+    padding-top: 1.5rem;
+  }
+
+  img {
+    width: 64px;
+    height: 64px;
+  }
+`
+export const ItemContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 0.5rem;
+`
+export const NameAndPrice = styled.div`
+  display: flex;
+  justify-content: space-between;
+  color: ${(props) => props.theme['base-subtitle']};
+  line-height: 130%;
+
+  span {
+    color: ${(props) => props.theme['base-text']};
+    text-align: right;
+    font-weight: 700;
+  }
+`
+export const Handlers = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  input {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 2rem;
+    width: 4.5rem;
+    padding: 0.5rem;
+    border-radius: 6px;
+    background: ${(props) => props.theme['base-button']};
+    border: none;
+
+    color: ${(props) => props.theme['base-title']};
+    text-align: center;
+    line-height: 130%;
+  }
+  input[type='number']::-webkit-inner-spin-button,
+  input[type='number']::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 2rem;
+    padding: 0rem 0.5rem;
+    gap: 0.25rem;
+    border: none;
+    cursor: pointer;
+
+    border-radius: 6px;
+    background: ${(props) => props.theme['base-button']};
+
+    color: ${(props) => props.theme['base-text']};
+    font-size: 0.75rem;
+    line-height: 0;
+
+    span {
+      color: ${(props) => props.theme.purple};
+    }
+  }
+`
+
+export const OrderValues = styled.div``
